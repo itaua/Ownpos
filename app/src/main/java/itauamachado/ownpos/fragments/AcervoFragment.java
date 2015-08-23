1b94ecb98efa0173e67631dda8f0b275aeed8b17
itaua <itauafm@gmail.com>
2015-08-23 18:02:12 -0300

    Update AcervoFragment.java

--- app/src/main/java/itauamachado/ownpos/fragments/AcervoFragment.java
+++ app/src/main/java/itauamachado/ownpos/fragments/AcervoFragment.java
@@ -1,1 +1,144 @@
 
+package itauamachado.ownpos.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+
+import itauamachado.ownpos.MainActivity;
+import itauamachado.ownpos.R;
+import itauamachado.ownpos.adapters.AcervoAdapter;
+import itauamachado.ownpos.domain.Acervo;
+import itauamachado.ownpos.interfaces.RecyclerViewOnClickListenerHack;
+
+public class AcervoFragment extends Fragment implements RecyclerViewOnClickListenerHack {
+
+    private RecyclerView mRecyclerView;
+    private List<Acervo> mList;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_acervo, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.rv_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager llm = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                AcervoAdapter adapter = (AcervoAdapter) mRecyclerView.getAdapter();
+
+                if (mList.size() == llm.findLastCompletelyVisibleItemPosition() + 1) {
+                    List<Acervo> listAux = ((MainActivity) getActivity()).getSetCarList(10);
+
+                    for (int i = 0; i < listAux.size(); i++) {
+                        adapter.addListItem(listAux.get(i), mList.size());
+                    }
+                }
+            }
+        });
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(getActivity(), mRecyclerView, this));
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+
+
+        mList = ((MainActivity) getActivity()).getSetCarList(10);
+        AcervoAdapter adapter = new AcervoAdapter(getActivity(), mList);
+        //adapter.setRecyclerViewOnClickListenerHack(this);
+        mRecyclerView.setAdapter( adapter );
+
+
+        return view;
+    }
+
+    @Override
+    public void onClickListener(View view, int position) {
+        Toast.makeText(getActivity(), "Position: "+position, Toast.LENGTH_SHORT).show();
+        AcervoAdapter adapter = (AcervoAdapter) mRecyclerView.getAdapter();
+        adapter.removeListItem(position);
+    }
+
+    @Override
+    public void onLongPressClickListener(View view, int position) {
+        Toast.makeText(getActivity(), "Position: "+position, Toast.LENGTH_SHORT).show();
+
+        AcervoAdapter adapter = (AcervoAdapter) mRecyclerView.getAdapter();
+        adapter.removeListItem(position);
+    }
+
+    public static class RecyclerViewTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private Context mContext;
+        private GestureDetector mGestureDetector;
+        private RecyclerViewOnClickListenerHack mRecyclerViewOnClickListenerHack;
+
+
+        public RecyclerViewTouchListener(Context c, final RecyclerView rv, RecyclerViewOnClickListenerHack rvH ){
+            mContext = c;
+            mRecyclerViewOnClickListenerHack = rvH;
+
+            mGestureDetector = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    View v = rv.findChildViewUnder(e.getX(), e.getY());
+                    if(v != null && mRecyclerViewOnClickListenerHack!=null){
+                        mRecyclerViewOnClickListenerHack.onClickListener(v,
+                                rv.getChildAdapterPosition(v));
+                    }
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View v = rv.findChildViewUnder(e.getX(), e.getY());
+
+                    if(v != null && mRecyclerViewOnClickListenerHack!=null){
+                        mRecyclerViewOnClickListenerHack.onLongPressClickListener(v,
+                                rv.getChildAdapterPosition(v));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            mGestureDetector.onTouchEvent(e);
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+}
+
