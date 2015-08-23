19314e48c9e133bda34a7b70f9670eade4653e6f
Itauafm <itauafm@gmail.com>
2015-08-23 17:51:07 -0300

    modificações no layout..

---
+++ app/src/main/java/itauamachado/ownpos/interfaces/RecyclerViewOnClickListenerHack.java
@@ -0,0 +1,11 @@
+package itauamachado.ownpos.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by Itauá on 22/08/2015.
+ */
+public interface RecyclerViewOnClickListenerHack {
+    public void onClickListener(View view, int position);
+    public void onLongPressClickListener(View view, int position);
+}
